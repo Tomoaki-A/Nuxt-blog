aa9b533d46e33aa41e8d9d4f7b6b579594768b61
Tomoaki Asano <tasano@vitalify.jp>
2021-12-31 18:29:10 +0900

    no message

---
+++ pages/_slug/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="body">
+    <Header />
+    <div class="title">
+      {{ title }}
+    </div>
+    <div class="sub-title">
+      {{ updatedAt }}
+    </div>
+    <div class="content">
+      {{ body }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://textnuxtblog.microcms.io/api/v1/posts/${params.slug}`,
+      {
+        headers: {
+          "X-MICROCMS-API-KEY": "5ebf93063f2a4772a4573bc16136551e3360",
+        },
+      }
+    );
+    console.log(data);
+    return data;
+  },
+};
+</script>
+
+<style scoped>
+.body {
+  background-color: #d1ecf7;
+  min-height: 100vh;
+  padding-bottom: 50px;
+}
+.title {
+  margin:100px 0  20px;
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+}
+.sub-title {
+  text-align: center;
+  color: gray;
+  margin-bottom: 40px;
+}
+.content {
+  background-color: #fff;
+  border-radius: 10px;
+  width: 750px;
+  min-height: 500px;
+  margin: 0 auto 30px;
+  padding: 30px;
+}
+</style>
---
+++ pages/grossery.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Header />
+    <div class="contents">
+      <div class="side-nav">
+        <SideNav />
+      </div>
+      <div class="main-contents"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Header from "../components/Header.vue";
+import SideNav from "../components/sideNav.vue";
+import Blog from "../components/blogContent.vue";
+
+export default {
+  components: {
+    Header,
+    SideNav,
+    Blog,
+  },
+};
+</script>
+
+<style scoped>
+.contents {
+  width: 1120px;
+  margin: 50px auto 0px;
+  display: flex;
+  justify-content: space-between;
+  border: solid 1px lightgray;
+}
+.side-nav {
+  width: 150px;
+  height: 500px;
+  border: solid 1px lightgray;
+}
+.main-contents {
+  width: 850px;
+  border: solid 1px lightgray;
+}
+</style>
